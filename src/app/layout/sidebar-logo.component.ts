a3f89e41beb35cb0a9caaaf720d388065c8b3599
Ricardo Coelho <ricardocunhacoelho@gmail.com>
2026-09-11 17:09:16 -0300

    ajustes
    ajustes

--- src/app/layout/sidebar-logo.component.ts
+++ src/app/layout/sidebar-logo.component.ts
@@ -4,8 +4,9 @@ import { RouterLink } from '@angular/router';
 @Component({
     selector: 'sidebar-logo',
     templateUrl: './sidebar-logo.component.html',
+    styleUrls: ['./sidebar-logo.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
     standalone: true,
     imports: [RouterLink],
 })
-export class SidebarLogoComponent {}
+export class SidebarLogoComponent { }
